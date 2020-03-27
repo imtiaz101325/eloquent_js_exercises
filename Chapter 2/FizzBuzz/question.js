50a096f9067708152cc229955a25bd5743ad936b
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-03-27 17:00:09 +0600

    updated FizzBuzz test

--- Chapter 2/FizzBuzz/question.js
+++ Chapter 2/FizzBuzz/question.js
@@ -1,13 +1,4 @@
 function FizzBuzz() {
-  for (let i=1; i<=100; i++) {
-    if (i%3 === 0 && i%5 === 0) {
-      console.log('FizzBuzz')
-    } else if (i%3 === 0) {
-      console.log('Fizz');
-    } else if (i%5 === 0) {
-      console.log('Buzz')
-    }
-  }
 }
 
 module.exports = FizzBuzz;
--- Chapter 2/FizzBuzz/test.js
+++ Chapter 2/FizzBuzz/test.js
@@ -5,29 +5,34 @@ describe(
     it('Prints FizzBuzz', () => {
         console.log = jest.fn();
         question();
-        
+
         let correct = true;
-        const ans = [...Array(100).keys()].map(num => num+1)
-        .filter(
-          num => num%3===0 || num%5 === 0
-        ).map(
-          i => {
-            if (i%3 === 0 && i%5 === 0) {
-              return 'FizzBuzz';
-            }
-            
-            if (i%3 === 0) {
-              return 'Fizz';
-            }
 
-            return 'Buzz';
-          }
-        );
+        if (console.log.mock.calls.length < 1) {
+          correct = false;
+        } else {
+          const ans = [...Array(100).keys()].map(num => num+1)
+          .filter(
+            num => num%3===0 || num%5 === 0
+          ).map(
+            i => {
+              if (i%3 === 0 && i%5 === 0) {
+                return 'FizzBuzz';
+              }
+              
+              if (i%3 === 0) {
+                return 'Fizz';
+              }
 
-        for (let i=0; i<console.log.mock.calls.length; i++) {
-          if (ans[i] !== console.log.mock.calls[i][0]) {
-            console.warn(`${i} is wrong. Expected ${ans[i]} got ${console.log.mock.calls[i][0]}`);
-            correct = false;
+              return 'Buzz';
+            }
+          );
+
+          for (let i=0; i<console.log.mock.calls.length; i++) {
+            if (ans[i] !== console.log.mock.calls[i][0]) {
+              console.warn(`${i} is wrong. Expected ${ans[i]} got ${console.log.mock.calls[i][0]}`);
+              correct = false;
+            }
           }
         }
 
