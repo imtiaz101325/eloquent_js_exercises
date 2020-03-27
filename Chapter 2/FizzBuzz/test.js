0a9da115e49ef5197bff17639499e9e7351c95ec
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-03-27 16:50:26 +0600

    Added FizzBuzz test

---
+++ Chapter 2/FizzBuzz/test.js
@@ -0,0 +1,37 @@
+const question = require('./question');
+
+describe(
+  'Print FizzBuzz from 1 to 100', () => {
+    it('Prints FizzBuzz', () => {
+        console.log = jest.fn();
+        question();
+        
+        let correct = true;
+        const ans = [...Array(100).keys()].map(num => num+1)
+        .filter(
+          num => num%3===0 || num%5 === 0
+        ).map(
+          i => {
+            if (i%3 === 0 && i%5 === 0) {
+              return 'FizzBuzz';
+            }
+            
+            if (i%3 === 0) {
+              return 'Fizz';
+            }
+
+            return 'Buzz';
+          }
+        );
+
+        for (let i=0; i<console.log.mock.calls.length; i++) {
+          if (ans[i] !== console.log.mock.calls[i][0]) {
+            console.warn(`${i} is wrong. Expected ${ans[i]} got ${console.log.mock.calls[i][0]}`);
+            correct = false;
+          }
+        }
+
+        expect(correct).toBe(true);
+    })
+  }
+)
